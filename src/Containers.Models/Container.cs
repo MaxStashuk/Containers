6be1e42e9209425271b8a7c966371dbefea1a4de
Maksym Stashuk <s30251@pjwstk.edu.pl>
2025-04-18 16:21:22 +0200

    Created models

--- src/Containers.Models/Container.cs
+++ src/Containers.Models/Container.cs
@@ -3,4 +3,9 @@
 public class Container
 {
     
+    public int Id { get; set; }
+    public int ContainerTypeId { get; set; }
+    public bool isHazardious { get; set; }
+    public string Name { get; set; }
+    
 }
